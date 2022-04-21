a7dec5bce2446db4ff37cce6d34887f745573ca4
Giantory <giantory.bryant@gmail.com>
2022-04-20 22:44:34 -0500

    feat(express-favs): add express server set up

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require("mongoose");
+
+// Server Setup
+const PORT = 3001;
+
+// Express Setup
+const app = express();
+app.use(express.json());
+
+mongoose.connect(
+	`mongodb://localhost:27017/favsDB`
+)
+.then(()=>{
+    app.listen(PORT, () => {
+        console.log(`App listening on ${PORT}`);
+    });
+    
+    // Routes
+    const lists = require('./express-favs/api/lists/routes');
+    const favs = require('./express-favs/api/favs/routes');
+    const users = require('./express-favs/api/users/routes');
+    
+    app.use('/api/lists', lists);
+    app.use('/api/favs', favs);
+    app.use('/api/users', users);
+})
+.catch((err) => {
+    console.log(err);
+  });
+
