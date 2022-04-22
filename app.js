f62031d2a4b3ecc42a7f6b7c1dfc10e09e1bb561
Giantory <giantory.bryant@gmail.com>
2022-04-22 15:15:43 -0500

    feat(express-favs): moduralize app instance

---
+++ app.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+
+
+// Express Setup
+const app = express();
+app.use(express.json());
+
+// Routes
+const favlists = require('./express-favs/api/favslists/routes');
+const favs = require('./express-favs/api/favs/routes');
+const users = require('./express-favs/api/users/routes');
+
+app.use('/api/favslists', favlists);
+app.use('/api/favs', favs);
+app.use('/api/users', users);
+
+
+module.exports = app
