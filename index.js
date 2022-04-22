169b0aef800ca57d7e5eebf1e44be02f319ac0c6
Giantory <giantory.bryant@gmail.com>
2022-04-22 15:16:33 -0500

    feat(express-favs): adapt index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const express = require('express');
+const app = require('./app')
 const mongoose = require("mongoose");
 
 // Server Setup
-const PORT = 3001;
 
-// Express Setup
-const app = express();
-app.use(express.json());
+const PORT = 3001
 
 mongoose.connect(
     `mongodb://localhost:27017/favsDB`
@@ -15,15 +12,7 @@ app.listen(PORT, () => {
     console.log(`App listening on ${PORT}`);
 });
 
-// Routes
-const favlists = require('./express-favs/api/favslists/routes');
-const favs = require('./express-favs/api/favs/routes');
-const users = require('./express-favs/api/users/routes');
-
-app.use('/api/favslists', favlists);
-app.use('/api/favs', favs);
-app.use('/api/users', users);
 
 
-module.exports = app
+
 
