6628049402b9d0f9e1e2cd52d32cde3e241c618b
Giantory <giantory.bryant@gmail.com>
2022-06-02 21:27:01 -0500

    fix(express-favs): change some routes according to challenge

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const favlists = require('./express-favs/api/favslists/routes');
 const favs = require('./express-favs/api/favs/routes');
 const users = require('./express-favs/api/users/routes');
 
-app.use('/api/favslists', favlists);
+app.use('/api/favs', favlists);
 app.use('/api/favs', favs);
-app.use('/api/users', users);
+app.use('/api/', users);
 
 
 module.exports = app
--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const mongoose = require("mongoose");
 // Server Setup
 
 const PORT = 3001
+MONGO_URI = `mongodb://localhost:27017/favsDB`
 
 mongoose.connect(
-    `mongodb://localhost:27017/favsDB`
+    MONGO_URI
 )
 app.listen(PORT, () => {
     console.log(`App listening on ${PORT}`);
